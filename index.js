4162876ed6c697b1130ca67bc6e8302f5c7ec3cf
AnhKhoa <anhkhoa1612vn@gmail.com>
2023-05-25 11:48:09 +0700

    update

--- index.js
+++ index.js
@@ -15,23 +15,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const ethers = require("ethers");
-// const API_URL = process.env.API_URL;
-const API_URL = "HTTP://127.0.0.1:7545";
-const PRIVATE_KEY = "bfccf6bbbc30bdf65f99fc5505f395c9fbd1f7d136b76be38a3f07cb39e30ac9";
-const contractAddress = "0xc1f239C3c77a7FD3126Ad3232C2042c5cCAFdE1D";
-const ADMIN_ADDRESS = "0x1ffC366b4626c0597216a6Df0AC1b4C87a30EB70";
-
-// connect to smart contract
-const {
-  abi
-} = require("./scripts/abi.json");
-
-const provider = new ethers.providers.JsonRpcProvider(API_URL);
-const signer = new ethers.Wallet(PRIVATE_KEY, provider);
-
-const contractInstance = new ethers.Contract(contractAddress, abi, signer);
-
 //Connect Mongoose
 mongoose
   .connect(process.env.MONGODB_URL, {
